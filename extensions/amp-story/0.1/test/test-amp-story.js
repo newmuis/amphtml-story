0dff0e892602f29339bd2005f883fc5e229359ed
Jonathan Newmuis <newmuis@google.com>
2017-04-12 11:16:46 -0700

    Add a base amp-story extension.

---
+++ extensions/amp-story/0.1/test/test-amp-story.js
@@ -0,0 +1,21 @@
+import {AmpStory} from '../amp-story';
+
+describes.realWin('amp-story', {
+  amp: {
+    extensions: ['amp-story'],
+  }
+}, env => {
+
+  let win;
+  let element;
+
+  beforeEach(() => {
+    win = env.win;
+    element = win.document.createElement('amp-story');
+    win.document.body.appendChild(element);
+  });
+
+  it('should build', () => {
+    element.build();
+  });
+});
