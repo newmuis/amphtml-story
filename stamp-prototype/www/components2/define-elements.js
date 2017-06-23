79c3a53334da33cac6de8a42d9bc6e87843e43be
Jon Newmuis <newmuis@google.com>
2017-06-23 09:29:24 -0700

    Remove STAMP prototype code.

--- stamp-prototype/www/components2/define-elements.js
+++
@@ -1,5 +0,0 @@
-customElements.define('amp-story-sequential-layer', AmpStorySequentialLayer);
-customElements.define('amp-story-fill-layer', AmpStoryFillLayer);
-customElements.define('amp-story-grid-layer', AmpStoryGridLayer);
-customElements.define('amp-story-page', AmpStoryPage);
-customElements.define('amp-story', AmpStory);
