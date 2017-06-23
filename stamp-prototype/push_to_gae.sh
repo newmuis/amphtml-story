8029a7e66455d65de9ad0a7f9f9c0e08840d1526
Jon Newmuis <newmuis@google.com>
2017-06-23 14:55:43 -0700

    Copy all files from dist to stamp-prototype/www.

--- stamp-prototype/push_to_gae.sh
+++ stamp-prototype/push_to_gae.sh
@@ -9,14 +9,8 @@ cd "`git rev-parse --show-toplevel`/stamp-prototype"
 # Build AMP and extensions.
 gulp dist
 
-# Copy relevant files to the stamp-prototype/www/ directory.
-cp -v ../dist/v0.css ./www/v0.css
-cp -v ../dist/v0.js ./www/v0.js
-cp -v ../dist/v0.js.map ./www/v0.js.map
-cp -v ../dist/v0/amp-story-0.1.css ./www/v0/amp-story-0.1.css
-cp -v ../dist/v0/amp-story-0.1.js ./www/v0/amp-story-0.1.js
-cp -v ../dist/v0/amp-story-0.1.js.map ./www/v0/amp-story-0.1.js.map
-cp -v ../dist/v0/amp-story-latest.js ./www/v0/amp-story-latest.js
+# Copy files to the stamp-prototype/www/ directory.
+cp -a -v ../dist/* ./www
 
 # Deploy the app to App Engine.
 gcloud app deploy app.yaml --no-promote --quiet --project=stamp-prototype
