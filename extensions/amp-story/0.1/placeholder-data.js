3e4598fc643e1a532b26835b24e47cbf05366dc1
Alan Orozco <orozcoalan@gmail.com>
2017-06-23 12:57:28 -0700

    Add basic markup and styling for bookend

---
+++ extensions/amp-story/0.1/placeholder-data.js
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2017 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// data structure does not necessarily match final
+export const DEMO_PLACEHOLDER_DATA = {
+  articles: [
+    [
+      {
+        title: 'This is India and the best places you should go',
+        domainName: 'apub.com',
+        readingTimeMins: 5,
+        image: {
+          url: 'http://placekitten.com/g/200/200',
+        },
+      },
+      {
+        title: 'A wonderful weekend with Tenturi',
+        domainName: 'apub.com',
+        readingTimeMins: 20,
+        image: {
+          url: 'http://placekitten.com/g/220/220',
+        },
+      },
+    ],
+    [
+      {
+        title: 'Examining the moon',
+        domainName: 'apub.com',
+        readingTimeMins: 45,
+      },
+      {
+        title: 'Architecture in the thousand-year capital',
+        domainName: 'apub.com',
+        readingTimeMins: 45,
+      },
+    ],
+  ],
+};
