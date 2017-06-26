ed7b462c5727a0b02df39c593e4b696ebb2a78bc
Jon Newmuis <newmuis@google.com>
2017-06-26 10:33:45 -0700

    Automatically convert cdn.ampproject.org references to stamp-prototype.appspot.com references in examples, during push

--- stamp-prototype/push_to_gae.sh
+++ stamp-prototype/push_to_gae.sh
@@ -15,5 +15,8 @@ cp -a -v ../dist/* ./www
 # Copy examples to the stamp-prototype/www/ directory.
 cp -a -v ../examples/stamp/* ./www
 
+# Replace 'cdn.ampproject.org' with 'stamp-prototype.appspot.com' in examples.
+find . -name '*.html' -exec sed -i '' -e 's/cdn.ampproject.org/stamp-prototype.appspot.com/g' {} \;
+
 # Deploy the app to App Engine.
 gcloud app deploy app.yaml --no-promote --quiet --project=stamp-prototype
