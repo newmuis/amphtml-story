39bb5ba3f83d31bb314ec2e58f0541aa7aecb1b4
Dima Voytenko <dvoytenko@google.com>
2017-06-30 16:44:21 -0700

    Force remeasures on font timeout/load (#10221)
    * Force remeasures on font timeout/load
    
    * lints

--- src/service/resources-impl.js
+++ src/service/resources-impl.js
@@ -24,6 +24,14 @@ import {TaskQueue} from './task-queue';
 import {VisibilityState} from '../visibility-state';
 import {checkAndFix as ieMediaCheckAndFix} from './ie-media-bug';
 import {closest, hasNextNodeInDocumentOrder} from '../dom';
+import {
+  documentInfoForDoc,
+  inputFor,
+  timerFor,
+  viewerForDoc,
+  viewportForDoc,
+  vsyncFor,
+} from '../services';
 import {expandLayoutRect} from '../layout-rect';
 import {installInputService} from '../input';
 import {loadPromise} from '../event-helper';
@@ -267,6 +275,22 @@ export class Resources {
           this.win.document.fonts.status != 'loaded') {
         this.win.document.fonts.ready.then(remeasure);
       }
+    });
+
+    // Safari 10 and under incorrectly estimates font spacing for `@font-face`
+    // fonts. This leads to wild measurement errors. The best course of action
+    // is to remeasure everything on window.onload or font timeout (3s),
+    // whichever is earlier. This has to be done on the global window because
+    // this is where the fonts are always added. Unfortunately,
+    // `document.fonts.ready` cannot be used here due to
+    // https://bugs.webkit.org/show_bug.cgi?id=174030.
+    // See https://bugs.webkit.org/show_bug.cgi?id=174031 for more details.
+    Promise.race([
+      loadPromise(this.win),
+      timerFor(this.win).promise(3100),
+    ]).then(() => {
+      this.relayoutAll_ = true;
+      this.schedulePass();
     });
   }
 
