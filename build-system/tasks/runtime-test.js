f8b68834d1cc4b76e6ce788fae858387976bb0a7
Raghu Simha <rsimha@ampproject.org>
2017-08-04 16:52:49 -0400

    Don't run gulp build for unit tests (#10792)

--- build-system/tasks/runtime-test.js
+++ build-system/tasks/runtime-test.js
@@ -167,7 +167,8 @@ function printArgvMessages() {
 /**
  * Run tests.
  */
-gulp.task('test', 'Runs tests', argv.nobuild ? [] : ['build'], function(done) {
+gulp.task('test', 'Runs tests',
+    argv.nobuild ? [] : (argv.unit ? ['css'] : ['build']), function(done) {
   if (!argv.integration && process.env.AMPSAUCE_REPO) {
     console./*OK*/info('Deactivated for ampsauce repo')
   }
