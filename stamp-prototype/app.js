e1ebc1f0eeeb69fe7aebbf373eb0377e79c05d19
Jon Newmuis <newmuis@google.com>
2017-08-14 10:50:00 -0400

    Add a version of the cities prototype that accesses the extension from the same origin that the story is served from.  Also add access control headers that properly reflect the origin.

--- stamp-prototype/app.js
+++ stamp-prototype/app.js
@@ -4,10 +4,16 @@ var express = require('express');
 var https = require('express-force-https');
 
 var app = express();
+var ORIGIN_REGEX = /^https?:\/\/[^\/]+/;
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  if (req.headers.referer) {
+    const origin = ORIGIN_REGEX.exec(req.headers.referer)[0];
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('AMP-Access-Control-Allow-Source-Origin', origin);
+    res.header('Access-Control-Expose-Headers', 'AMP-Access-Control-Allow-Source-Origin');
+  }
   next();
 });
 app.use(express.static('www'));
