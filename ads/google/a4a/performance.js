ampproject/amphtml

Performance: separate ini-load from first visible ini-load (#9665)

ads/google/a4a/performance.js
@@ -301,11 +301,17 @@ export class GoogleAdLifecycleReporter extends BaseLifecycleReporter {
                         () => {
                           this.sendPing('firstVisible');
                         });
-      // ini-load
+
+      // ini load
+      readyPromise.then(() => {
+        this.sendPing('iniLoad');
+      });
+
+      // first visible + ini-load
       vis.listenElement(element, {waitFor: 'ini-load'},
                         readyPromise, null,
                         () => {
-                          this.sendPing('iniLoad');
+                          this.sendPing('visIniLoad');
                         });
 
       // 50% vis, ini-load and 1 sec

newmuis/amphtml-story

ads/google/a4a/performance.js
@@ -299,9 +299,9 @@ export class GoogleAdLifecycleReporter extends BaseLifecycleReporter {
           });
       // first visible
       vis.listenElement(element, {visiblePercentageMin: 1}, null, null,
-          () => {
-            this.sendPing('firstVisible');
-          });
+                        () => {
+                          this.sendPing('firstVisible');
+                        });
 
       // ini load
       readyPromise.then(() => {
@@ -310,10 +310,10 @@ export class GoogleAdLifecycleReporter extends BaseLifecycleReporter {
 
       // first visible + ini-load
       vis.listenElement(element, {waitFor: 'ini-load'},
-          readyPromise, null,
-          () => {
-            this.sendPing('visIniLoad');
-          });
+                        readyPromise, null,
+                        () => {
+                          this.sendPing('visIniLoad');
+                        });
 
       // 50% vis, ini-load and 1 sec
       vis.listenElement(element,