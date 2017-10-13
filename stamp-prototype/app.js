65d73edab2032d3252c12a2ac8678193034b105c
Jon Newmuis <newmuis@users.noreply.github.com>
2017-10-12 20:36:51 -0400

    Hoist the poster image to the fill layer, in case the media disappears (e.g. during video loading in Android WebViews). (#193)

--- stamp-prototype/app.js
+++ stamp-prototype/app.js
@@ -14,6 +14,7 @@ var sendResponse = function(res, statusCode, body) {
   res.status(statusCode).send(body);
 };
 
+// Test new versions of the runtime on pages pointing to the production version.
 app.get('/test/*', function(req, res) {
   var url = req.params[0];
   if (!url) {
@@ -56,6 +57,57 @@ app.get('/test/*', function(req, res) {
       });
 });
 
+// Fake cache.
+app.get('/v/*', function(req, res) {
+  var path = req.params[0];
+  if (!path) {
+    sendResponse(res, 400, 'Must specify document URL.');
+    return;
+  }
+
+  var isSecureUrl = function(urlStr) {
+    return urlStr.startsWith('s/') || urlStr.startsWith('S/');
+  };
+  var urlScheme = isSecureUrl(path) ? 'https://' : 'http://';
+  var urlWithoutScheme = isSecureUrl(path) ? path.substring(2) : path;
+  var url = urlScheme + urlWithoutScheme;
+
+  fetch(url)
+      .then(function(fetchResponse) {
+        var contentType = fetchResponse.headers.get(CONTENT_TYPE_HEADER_NAME);
+        res.setHeader(CONTENT_TYPE_HEADER_NAME, contentType);
+
+        if (!contentType.indexOf('text/html') === 0) {
+          // For content types that we don't handle, just directly proxy the
+          // resource without doing anything additional.
+          fetchResponse.body.pipe(res);
+          return;
+        }
+
+        fetchResponse.text().then(function(incomingHtml) {
+          console.log(req);
+          var currentHost = 'https://' + req.headers.host;
+          var baseUrl = url.substring(0, url.lastIndexOf('/')) + '/';
+          var outgoingHtml = incomingHtml;
+          var v0host = /(http.*)v0.js/.exec(incomingHtml)[1];
+          outgoingHtml = outgoingHtml
+              // Add a base href to avoid proxying dependencies.
+              .replace(/<html(.*?)>/, '<html$1><base href="' + baseUrl + '">')
+
+              // Add the viewer integration script.
+              .replace('</head>', '<script async custom-element="amp-viewer-integration" src="' + v0host + 'v0/amp-viewer-integration-0.1.js"></script></head>');
+
+          // Send the response.
+          sendResponse(res, 200, outgoingHtml);
+          return;
+        }).catch(function(error) {
+          sendResponse(res, 500, error);
+        });
+      }).catch(function(error) {
+        sendResponse(res, 500, error);
+      });
+});
+
 app.use(function(req, res, next) {
   if (req.headers.referer) {
     const origin = ORIGIN_REGEX.exec(req.headers.referer)[0];
@@ -66,7 +118,7 @@ app.use(function(req, res, next) {
   }
   next();
 });
-app.use(express.static('www'));
+app.use(express.static('www', { maxage: '600s' }));
 app.use(https);
 
 // Start the server
